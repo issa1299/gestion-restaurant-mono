5bd652a437ea1aa1c69deacd4723c7dca8160dfd
issa1299 <votre@email.com>
2026-08-05 08:47:14 +0000

    Fix: createsuperuser cree un superuser avec role ADMIN

--- apps/accounts/managers.py
+++ apps/accounts/managers.py
@@ -25,5 +25,6 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", "ADMIN")
 
         return self.create_user(username, email, password, **extra_fields)
